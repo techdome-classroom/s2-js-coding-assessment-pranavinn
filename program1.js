12194edc5842ece0fb429dc781ec08534fcaa23c
pranavinn <128679594+pranavinn@users.noreply.github.com>
2024-11-11 15:16:08 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,9 +3,23 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+    const stack = [];
+    const brackets = { ')': '(', '}': '{', ']': '[' };
+
+    for (let char of s) {
+        if (char in brackets) {
+            if (!stack.length || stack.pop() !== brackets[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
     
+    return stack.length === 0;
 };
 
 module.exports = { isValid };
+
 
 
